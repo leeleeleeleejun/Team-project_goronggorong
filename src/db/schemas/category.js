c3dda2bd67918ba5b5347f787a303829917d9376
elice72 <elice72@elice72ui-iMac.local>
2023-04-18 17:11:02 +0900

    Add:feature-BE-category-schema

---
+++ src/db/schemas/category.js
@@ -0,0 +1,11 @@
+import { Schema } from 'mongoose';
+
+const categorySchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+    }
+)
+export { categorySchema }
