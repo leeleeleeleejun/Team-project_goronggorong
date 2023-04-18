d6d919467266c558342c3c3d80f8333a0f657c05
Sujin Jeon <mackerel0106@gmail.com>
2023-04-18 14:35:50 +0900

    Feat: Logger 생성
    utils/logger.js 생성
    morgan, winston import

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+// import morgan from 'morgan';
+
+// import { httpLogStream } from './utils/index.js';
+
+// process.env에 설정한 환경변수 추가
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,6 +13,8 @@ const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('root');
 });
+
+// app.use(morgan('dev', { stream: httpLogStream }));
 
 app.listen(port, () => {
     console.log(`Connected to ${port}...`);
