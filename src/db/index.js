68b33e4f30e88c85cdaf25365e1ec48315a05480
Sujin Jeon <mackerel0106@gmail.com>
2023-04-18 18:32:34 +0900

    Add: index.js 추가
    모든 폴더에 index.js 추가적으로 생성

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.DB_KEY);
+// mongoose.connect(process.env.DB_KEY);
+console.log(process.env.DB_KEY);
 
 const db = mongoose.connection;
 
 db.on('connected', () => console.log('Connecting DB Success'));
-db.on('error', (err) => console.err('Connecting DB Failed'));
+db.on('error', (err) => console.error('Connecting DB Failed'));
 
-export * from './models';
+export * from './models/index.js';
