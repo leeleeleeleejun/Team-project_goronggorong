57f14821f0f4e6ff29c0b7860c630424d3ec6e30
Sujin Jeon <mackerel0106@gmail.com>
2023-04-18 12:32:43 +0900

    Feat: 서버 port 연결 후 listen
    app.js에 서버 동작할 수 있게 코드 추가

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('root');
+});
+
+app.listen(port, () => {
+    console.log(`Connected to ${port}...`);
+});
