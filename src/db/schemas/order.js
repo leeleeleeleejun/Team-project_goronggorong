2cf761e0d8570a0792fb77cc1aec082e96ccb82c
Sujin Jeon <mackerel0106@gmail.com>
2023-04-18 15:49:01 +0900

    Feat: OrderSchema 생성
    mongodb 연결
    OrderSchema 생성

---
+++ src/db/schemas/order.js
@@ -0,0 +1,22 @@
+import { Schema } from 'mongoose';
+
+const OrderSchema = new Schema({
+  /*
+    User : name, phone_number, address
+  */
+  productList: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+    },
+  ],
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  deliveryStatus: {
+    type: String,
+    required: true,
+  },
+});
