72c0f471698020851ade411e1721e3f80c2f09d9
Sujin Jeon <mackerel0106@gmail.com>
2023-04-19 21:20:16 +0900

    Add: User CRUD router 생성
    postman으로 샘플 데이터 확인 완료
    schema phoneNumber phone으로 변경
    logger 추가

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,4 +2,4 @@ import httpLogStream from './logger.js';
 import asyncHandler from './async-handler.js';
 import hashPassword from './hash-password.js';
 
-export { httpLogStream, asyncHandler, hashPassword };
+export { httpLogStreams, asyncHandler, hashPassword };
