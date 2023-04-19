6ced0139f339b33bafc236465969f65cdeb550f4
elice70 <elice70@elice70ui-iMac.local>
2023-04-19 21:24:07 +0900

    Fix: product router 고치는중

--- src/routers/product-router.js
+++ src/routers/product-router.js
@@ -2,11 +2,11 @@ import { Router } from 'express';
 import { productService } from '../services/product-service.js'
 const productRouter = Router();
 
-productRouter.get('/products', async (req, res, next) => {
+productRouter.get('/', async (req, res, next) => {
     try {
-      // 전체 제품 목록을 얻음
+      // 전체 제품 목록 불러오기
       const products = await productService.getProducts();
-      // 제품 목록(배열)을 JSON 형태로 프론트에 보냄
+      // 제품 목록 JSON 형태로 프론트에 쏴주기
       res.status(200).json(products);
     } catch(error) {
       next(error);
@@ -26,6 +26,7 @@ productRouter.get('/products/:category', async (req, res, next) => {
 
 productRouter.get('/products/:category/:id', async (req, res, next) => {
   try {
+    // 아이디로 찾아서 JSON 으로 프론트에 쏴주기
     const { category, id } = req.params;
     const product = await productService.getProductById(id);
     res.status(200).json(product);
@@ -33,6 +34,16 @@ productRouter.get('/products/:category/:id', async (req, res, next) => {
     next(error);
   }
 });
+
+productRouter.post('/productregister', async (req, res, next) => {
+  try {
+    const newProduct = await productService.addProduct(req.body)
+    res.status(200).json(product);
+  } catch(error) {
+    next(error);
+  }
+
+})
 
 
 export { productRouter };
