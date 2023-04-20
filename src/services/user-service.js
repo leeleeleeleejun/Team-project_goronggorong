679387841336d939c3e5804e60bcb6ecee1bcbb6
Sujin Jeon <mackerel0106@gmail.com>
2023-04-20 15:01:19 +0900

    Add: password hash하는 기능 추가
    user-service 생성
    기존 라우터에서 하던 일들 다 user-service로 이동 후 메서드 새로 생성
    bcrypt 이용해서 hashpassword 생성

---
+++ src/services/user-service.js
@@ -0,0 +1,40 @@
+import bcrypt from 'bcrypt';
+import { userModel } from '../db/models/index.js';
+
+class UserService {
+  constructor(userModel) {
+    this.userModel = userModel;
+  }
+
+  async createUser(userInfo) {
+    const { name, email, password, phone, address } = userInfo;
+    const hashedPassword = await userService.createHashPassword(password);
+    const user = await this.userModel.createUser({ name, email, password: hashedPassword, phone, address });
+
+    return user;
+  }
+
+  // bcrypt
+  async createHashPassword(password) {
+    const salt = 12;
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    return hashedPassword;
+  }
+
+  async isValidPassword(password) {
+    const compareResult = bcrypt.compare(password, this.userModel.password);
+
+    return compareResult;
+  }
+}
+
+const userService = new UserService(userModel);
+
+export default userService;
+
+/* 
+  bcrypt
+  .hash: hash password 생성
+  .compare: hashpassword 비교
+*/
