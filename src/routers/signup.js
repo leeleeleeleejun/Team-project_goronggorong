0326ae60d56d3f7c67176a14a25e6d7f90c4e9fb
Sujin Jeon <mackerel0106@gmail.com>
2023-04-20 12:20:53 +0900

    Add: asyncHandler 생성 및 update delete 기능 삭제
    router return하는 방식 수정
    asyncHandler 사용해서 async 함수들 처리

--- src/routers/signup.js
+++ src/routers/signup.js
@@ -1,70 +1,39 @@
 import { Router } from 'express';
 import { userModel } from '../db/models/index.js';
+import { asyncHandler } from '../utils/index.js';
 
 const router = Router();
 
-router.get('/', async (req, res, next) => {
-  const users = await userModel.findAll();
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const users = await userModel.findAll();
 
-  if (!users) {
-    res.send('signup');
-    return;
-  } else {
-    return res.json({
-      code: 200,
-      message: '전체 User 목록을 읽어왔습니다.',
-      users,
-    });
-  }
-});
+    if (!users) {
+      return res.status(200).json({
+        message: '생성된 User가 없습니다.',
+      });
+    } else {
+      return res.status(200).json({
+        message: '전체 User 목록을 읽어왔습니다.',
+        users,
+      });
+    }
+  }),
+);
 
-router.post('/', async (req, res, next) => {
-  const { name, email, password, phone, address } = req.body;
+router.post(
+  '/',
+  asyncHandler(async (req, res) => {
+    const { name, email, password, phone, address } = req.body;
 
-  try {
     const user = await userModel.createUser({ name, email, password, phone, address });
 
-    return res.json({
-      code: 200,
+    return res.status(200).json({
       message: 'User가 생성됐습니다.',
       user,
     });
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.put('/:id', async (req, res, next) => {
-  const { id } = req.params;
-  const { name, email, password, phone, address } = req.body;
-
-  try {
-    const updatedUser = await userModel.updateUser(id, req.body);
-
-    return res.json({
-      code: 200,
-      message: '사용자 정보가 수정됐습니다.',
-      updatedUser,
-    });
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.delete('/:id', async (req, res, next) => {
-  const { id } = req.params;
-
-  try {
-    const deletedUser = await userModel.deleteUser(id);
-
-    return res.json({
-      code: 200,
-      message: '사용자가 삭제됐습니다',
-      deletedUser,
-    });
-  } catch (err) {
-    next(err);
-  }
-});
+  }),
+);
 
 export { router as signupRouter };
