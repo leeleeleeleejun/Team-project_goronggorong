0897f1af84ff4f211551af9028a610497a2b7ec8
[SW4/2반] 김차미 <dearcham2@gmail.com>
2023-04-21 20:37:17 +0900

    Add: sign-in.js 파일 제작
    로그인 로직 구현

---
+++ src/views/user/sign-in/sign-in.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const form = document.querySelector('.sign__form')
+const id = document.querySelector('.form__id')
+const pw = document.querySelector('.form__pw')
+
+form.addEventListener('submit',onSubmit)
+
+function onSubmit(e){
+  e.preventDefault();
+
+  axios.post('/signin',{
+    email : id.value.trim(),
+    password : pw.value.trim()
+  })
+  .then((res)=>{
+    sessionStorage.setItem("token",res.data)
+  })
+  .catch((err)=>{
+    alert(err)
+  })
+}
