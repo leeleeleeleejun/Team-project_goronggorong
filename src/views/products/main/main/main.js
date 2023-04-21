1b8dbec3931543b1f649ad56340ac8eb4d3aaf17
[SW4/2반] 김차미 <dearcham2@gmail.com>
2023-04-22 00:38:13 +0900

    Fix: 상품 리스트 div태그를 ul태그로 변경
    리스트 태그 사용 및 css, js 수정

--- src/views/products/main/main/main.js
+++ src/views/products/main/main/main.js
@@ -1,11 +1,11 @@
 //상품 불러오기
-const createItem = (item)=>{
+const createItem = (item) => {
   return `
-  <div class="prod__item">
+  <li class="prod__item">
             <a class="prod__link" href="/products/:${item.category}/${item.id}">
               <img
                 class="prod__link-thumb"
-                src="${item.imgUrl}"
+                src="https://storage.googleapis.com/hochony/gorongImg/gorongfood.jpeg"
               />
               <div class="prod__info">
                 <p class="prod__title">${item.name}</p>
@@ -15,18 +15,19 @@ const createItem = (item)=>{
                 </div>
               </div>
             </a>
-          </div>`
-}
+          </li>`;
+};
 
 //전체 상품 목록 불러오기
-axios.get('http://localhost:3001/items/')
-  .then((res)=>{
+axios
+  .get('http://localhost:3001/items/')
+  .then((res) => {
     const items = res.data;
-    const list = document.querySelector('.prod__list')
-    items.forEach((item)=>{
+    const list = document.querySelector('.prod__list');
+    items.forEach((item) => {
       list.innerHTML += createItem(item);
-    })
+    });
   })
-  .catch((err)=>{
-    alert(err)
-  })
+  .catch((err) => {
+    alert(err);
+  });
