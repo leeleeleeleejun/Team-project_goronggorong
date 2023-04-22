b85f4c53cc2f33936a94a8798687c24e03adf7a6
[SW4/2반] 김차미 <dearcham2@gmail.com>
2023-04-22 18:40:52 +0900

    Fix: detail.js 장바구니 담기 로직 수정
    로컬스토리지의 기존 데이터 확인 후 추가하는 방식으로 변경

--- src/views/detail/detail.js
+++ src/views/detail/detail.js
@@ -1,38 +1,44 @@
-axios.get('http://localhost:3000/items/:id')
-  .then((res)=>{
-    const item = res.data;   
-    const img = document.querySelector('.item__img')
-    const name = document.querySelector('.item__name')
-    const price = document.querySelector('.item__price')
-    const contentImg = document.querySelector('.content__img')
-    const navAmount= document.querySelector('.bottom-nav__amount--count')
-    const navCartBtn = document.querySelector('.bottom-nav__btn--cart')
+axios
+  .get('http://localhost:3000/items/:id')
+  .then((res) => {
+    const item = res.data;
 
-    img.src = item.imgUrl;
+    const itemImg = document.querySelector('.item__img');
+    const name = document.querySelector('.item__name');
+    const price = document.querySelector('.item__price');
+    const contentImg = document.querySelector('.content__img');
+    const navAmount = document.querySelector('.bottom-nav__amount--count');
+    const navCartBtn = document.querySelector('.bottom-nav__btn--cart');
+
+    itemImg.setAttribute('src', item.imgUrl);
     name.innerHTML = item.name;
     price.innerHTML = item.price;
     //상품상세이미지로 바꿔둘 것
-    contentImg.src = item.imgUrl;
+    contentImg.setAttribute('src', item.imgUrl);
 
-    let cartItem = []
+    navCartBtn.addEventListener('click', addCart);
 
-    navCartBtn.addEventListener('click',addCart)
+    let cartItem = [];
 
-    function addCart(e){
-      const newCart = {
+    function addCart() {
+      const newItem = {
         id: new Date(),
-        img : item.imgUrl,
-        name : item.name,
-        price : item.price,
-        amount : navAmount.value,
+        img: item.imgUrl,
+        name: item.name,
+        price: item.price,
+        amount: navAmount.value,
+      };
+      //만약 기존 추가된 아이템이 있다면
+      if (localStorage.getItem('cart')) {
+        cartItem = JSON.parse(localStorage.getItem('cart'));
       }
-      cartItem.push(newCart)
-      localStorage.setItem("cart",JSON.stringify(cartItem))
-      }
-    })
-    .catch((err)=>{
-      alert(err)
-    })
 
-
-
+      cartItem.push(newItem);
+      localStorage.setItem('cart', JSON.stringify(cartItem));
+      //배열 초기화
+      cartItem = [];
+    }
+  })
+  .catch((err) => {
+    alert(err);
+  });
