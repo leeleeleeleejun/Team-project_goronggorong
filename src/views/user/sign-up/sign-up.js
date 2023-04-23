ca9f24df752e68ab7ee8d207319bc1c2cc19f7dd
[SW4/2반] 김차미 <dearcham2@gmail.com>
2023-04-24 03:07:07 +0900

    Add: sign-up.js 회원가입 로직 추가
    - 회원가입 post요청
    - 로그인페이지로 리다이렉션

---
+++ src/views/user/sign-up/sign-up.js
@@ -0,0 +1,27 @@
+const userName = document.querySelector('.form__name');
+const id = document.querySelector('.form__id');
+const pw = document.querySelector('.form__pw');
+const phone = document.querySelector('.form__phone');
+const address = document.querySelector('.form__address');
+const submit = document.querySelector('.form__submit');
+
+submit.addEventListener('click', function (e) {
+  e.preventDefault();
+  axios
+    .post('http://localhost:3000/signup', {
+      name: userName.value,
+      email: id.value,
+      password: pw.value,
+      phone: phone.value,
+      address: address.value,
+    })
+    .then((res) => {
+      if (res.status === 201) {
+        alert(`
+        성공적으로 회원가입되었어요🎉
+        로그인 페이지로 이동합니다.`);
+        //로그인페이지로 이동
+        window.location.href = '/signin';
+      }
+    });
+});
