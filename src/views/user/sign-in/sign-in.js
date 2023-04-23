30c987bde65f64703662f1bed8f4c89d2dc2a877
[SW4/2반] 김차미 <dearcham2@gmail.com>
2023-04-24 00:26:31 +0900

    Fix: signin페이지 응답 코드 수정
    res.info === 200 조건 추가

--- src/views/user/sign-in/sign-in.js
+++ src/views/user/sign-in/sign-in.js
@@ -1,20 +1,23 @@
-const form = document.querySelector('.sign__form')
-const id = document.querySelector('.form__id')
-const pw = document.querySelector('.form__pw')
+const form = document.querySelector('.sign__form');
+const id = document.querySelector('.form__id');
+const pw = document.querySelector('.form__pw');
 
-form.addEventListener('submit',onSubmit)
+form.addEventListener('submit', onSubmit);
 
-function onSubmit(e){
+function onSubmit(e) {
   e.preventDefault();
 
-  axios.post('/signin',{
-    email : id.value.trim(),
-    password : pw.value.trim()
-  })
-  .then((res)=>{
-    sessionStorage.setItem("token",res.data)
-  })
-  .catch((err)=>{
-    alert(err)
-  })
+  axios
+    .post('/signin', {
+      email: id.value.trim(),
+      password: pw.value.trim(),
+    })
+    .then((res) => {
+      if (res.info === 200) {
+        localStorage.setItem('userToken', res.data);
+      }
+    })
+    .catch((err) => {
+      alert(err);
+    });
 }
