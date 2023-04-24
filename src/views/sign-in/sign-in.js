4bb505d5f28ae51da8764fe59d3ff0115e966876
[SW4/2반] 김차미 <dearcham2@gmail.com>
2023-04-25 00:41:03 +0900

    Fix: img url 수정
    img 경로 수정함

--- src/views/sign-in/sign-in.js
+++ src/views/sign-in/sign-in.js
@@ -2,13 +2,6 @@ const form = document.querySelector('.sign__form');
 const id = document.querySelector('.form__id');
 const pw = document.querySelector('.form__pw');
 
-// pw.addEventListener('paste',function(e){
-//   e.preventDefault()
-//   const clipboardData = e.clipboardData || window.clipboardData;
-//   var pastedData = clipboardData.getData('text');
-//   pw.value = pastedData;
-// })
-
 form.addEventListener('submit', function (e) {
   e.preventDefault();
 
