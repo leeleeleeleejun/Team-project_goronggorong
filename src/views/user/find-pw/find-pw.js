a90fda10eb20f3aaa843159dbdf0cdc1c0246efa
[SW4/2반] 김차미 <dearcham2@gmail.com>
2023-04-24 17:15:44 +0900

    Add: find-pw페이지 비밀번호 찾기 로직 추가
    비번찾기 시 서버에서 비번 재설정 후 모달창 띄움

---
+++ src/views/user/find-pw/find-pw.js
@@ -0,0 +1,43 @@
+const userName = document.querySelector('.form__name');
+const id = document.querySelector('.form__id');
+const phone = document.querySelector('.form__phone');
+const submitBtn = document.querySelector('.form__submit');
+// 모달관련 dom요소
+const modal = document.querySelector('.modal');
+const exitBtn = document.querySelector('.delete');
+const copyBtn = document.querySelector('.modal-copy');
+const signinBtn = document.querySelector('.modal-signin');
+const newPw = document.querySelector('.new-pw');
+
+submitBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  axios
+    .get('http://localhost:3000/find-password', {
+      name: userName.value,
+      email: id.value,
+      phone: phone.value,
+    })
+    .then((res) => {
+      if (res.status === 200) {
+        modal.classList.add('is-active');
+        newPw.value = res.data.info.password;
+      }
+      if (res.status === 400) {
+        alert(res.data.message);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+//모달 관련 이벤트
+exitBtn.addEventListener('click', function () {
+  modal.classList.remove('is-active');
+});
+copyBtn.addEventListener('click', function () {
+  navigator.clipboard.writeText(newPw.value);
+});
+signinBtn.addEventListener('click', function () {
+  window.location.href = '/signin';
+});
