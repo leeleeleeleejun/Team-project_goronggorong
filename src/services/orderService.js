7bf6d2d51bd45340effc4b85418d89651fb2a8be
Sujin Jeon <mackerel0106@gmail.com>
2023-04-25 00:26:26 +0900

    Add: createOrderId 로직 분리
    orderService에 createOrder
    orderService에 createOrderId

---
+++ src/services/orderService.js
@@ -0,0 +1,39 @@
+import { userModel, orderModel } from '../db/index.js';
+import { customError } from '../middlewares/index.js';
+
+const orderService = {
+  createOrderId: () => {
+    const now = new Date();
+    let year = String(now.getFullYear());
+    const month = String(now.getMonth() + 1);
+    const date = String(now.getDate());
+    if (month.length === 1) year += '0';
+    let orderId = year + month + date;
+
+    for (let i = 0; i < 10; i++) {
+      orderId += Math.floor(Math.random() * 10);
+    }
+
+    return orderId;
+  },
+  createOrder: async (orderInfo) => {
+    const checkUser = await userModel.findById(orderInfo.user);
+    if (!checkUser) {
+      throw new customError(400, '사용자가 없습니다.');
+    }
+
+    orderInfo.orderId = orderService.createOrderId();
+    if (orderInfo.paymentMethod.paymentType === 'card') {
+      orderInfo.deliveryStatus = '결제완료';
+    }
+
+    const order = await orderModel.createOrder(orderInfo);
+    if (!order) {
+      throw new customError(400, '주문이 완료되지 않았습니다.');
+    }
+
+    return order;
+  },
+};
+
+export default orderService;
