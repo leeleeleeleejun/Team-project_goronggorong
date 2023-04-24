12b0d911a3785c97d49dfa318711d1f29bbc9645
Sujin Jeon <mackerel0106@gmail.com>
2023-04-24 17:12:15 +0900

    Delete: .DS_Store Rename: cancle 오타 수정
    폴더이름 오타수정
    .DS_Store 외에 필요없는 파일들 삭제

--- src/services/viewService.js
+++ src/services/viewService.js
@@ -7,8 +7,6 @@ const viewService = {
     const src = path.join(__dirname, `../views/${srcFolder}`);
     const option = { index: `${srcFolder}.html` };
 
-    console.log(src);
-
     return express.static(src, option);
   },
 };
