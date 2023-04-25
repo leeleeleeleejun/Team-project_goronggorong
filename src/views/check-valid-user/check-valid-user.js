f386efdef5b9219b2f4366fa936257382b21b0c1
[SW4/2반] 김차미 <dearcham2@gmail.com>
2023-04-25 14:20:08 +0900

    Fix: mypage 헤더 푸터 변경
    layouts의 헤더 푸터로 변경

--- src/views/check-valid-user/check-valid-user.js
+++ src/views/check-valid-user/check-valid-user.js
@@ -1,21 +1,17 @@
-<<<<<<< HEAD
 const pw = document.querySelector('.form__pw');
 const submit = document.querySelector('.form__submit');
 
 submit.addEventListener('click', function (e) {
   e.preventDefault();
   axios
-  .get('/api/mypage/check-valid-user')
+    .get('/api/mypage/check-valid-user')
     .then((res) => {
       if (res.status === 201) {
         //회원정보수정 페이지로 이동
         window.location.href = '/mypage/edit-user-info';
       }
     })
-    .catch((err)=>{
-      alert(err.message)
-    })
+    .catch((err) => {
+      alert(err.message);
+    });
 });
-=======
-axios.get('/api/mypage/check-valid-user')
->>>>>>> eeac1e6 (Add: 카테고리제외 view라우터 연결)
