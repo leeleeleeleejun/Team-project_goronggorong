8a509f6051278386f1c7038453bfaadad7c0f99e
[SW4/2반] 김차미 <dearcham2@gmail.com>
2023-04-26 14:49:12 +0900

    Fix: check valid user 응답코드, 헤더 수정
    201->200으로 변경 / 헤더에 토큰보내기

--- src/views/check-valid-user/check-valid-user.js
+++ src/views/check-valid-user/check-valid-user.js
@@ -3,10 +3,19 @@ const submit = document.querySelector('.form__submit');
 
 submit.addEventListener('click', function (e) {
   e.preventDefault();
-  axios
-    .get('/api/mypage/check-valid-user')
+  const userToken = localStorage.getItem('userToken');
+  axios({
+    method: 'post',
+    url: '/api/mypage/check-valid-user',
+    headers: {
+      Authorization: `Bearer ${userToken}`,
+    },
+    data: {
+      password: pw.value,
+    },
+  })
     .then((res) => {
-      if (res.status === 201) {
+      if (res.status === 200) {
         //회원정보수정 페이지로 이동
         window.location.href = '/mypage/edit-user-info';
       }
